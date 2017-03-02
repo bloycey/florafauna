83c776169846dd72190393bf8ceb9a2fe11448fb
bloycey <bloyce1990@gmail.com>
2017-03-02 22:02:53 +1000

    slider draft

--- js/main.js
+++ js/main.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
         },
         function() { $('.dropdown-menu', this).stop().fadeOut("fast");
     });
+    
+     $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    infinite: true,
+    arrows: false,
+  });
+    
 });
+
