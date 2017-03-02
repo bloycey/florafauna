7820c69caa1d6119d71407d27424d45b745841a6
bloycey <bloyce1990@gmail.com>
2017-03-02 23:15:55 +1000

    Switched to BXslider - much better

--- js/main.js
+++ js/main.js
@@ -5,16 +5,12 @@ $(document).ready(function(){
         function() { $('.dropdown-menu', this).stop().fadeOut("fast");
     });
     
-     $('.slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    autoplay: true,
-    infinite: true,
-    arrows: false,
-  });
+    $('.bxslider').bxSlider({
+        auto: true,
+        infinite: true,
+        pager: false,
+        speed: 1000,
+    });
     
 });
 
