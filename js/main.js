bcc4741a877736c5c03359f1b11ef81c9a049325
bloycey <bloyce1990@gmail.com>
2017-03-05 20:45:37 +1000

    clickable menu on mobile

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
    
     
-    $(".dropdown").hover(
-        function() { 
+    $(".dropdown").bind('click hover', function() { 
             
             $('.dropdown-menu', this).stop().fadeIn("fast");
         },
+        
         function() { 
             
             $('.dropdown-menu', this).stop().fadeOut("fast");
